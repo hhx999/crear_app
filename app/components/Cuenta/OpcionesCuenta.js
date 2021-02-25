c3a7dad51b706ea38da8ff6b0ad9606fe937ddff
Borjas Vega Riveras <borjas.vegariveras@gmail.com>
2021-02-25 18:06:23 -0300

    TG-45 #closed Agregando modal dinamico para actualización de datos

--- app/components/Cuenta/OpcionesCuenta.js
+++ app/components/Cuenta/OpcionesCuenta.js
@@ -1,12 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 import { StyleSheet, View, Text } from "react-native";
 import { ListItem } from "react-native-elements";
 import { map } from "lodash";
+import Modal from "../Modal"
 
 export default function OpcionesCuenta(props) {
     
     const { userInfo, toastRef } = props;
-    const opcionesMenu = generarOpciones();
+    const [mostrarModal, setMostrarModal] = useState(false);
+    const [renderComponente, setRenderComponente] = useState(null)
+
+    const seleccionarComponente = (key) => {
+        switch (key) {
+            case "displayName":
+                setRenderComponente(
+                    <Text>
+                        Cambiando nombre y apellido
+                    </Text>
+                );
+                setMostrarModal(true);
+                break;
+            case "email":
+                setRenderComponente(
+                    <Text>
+                        Cambiando email
+                    </Text>
+                );
+                setMostrarModal(true);
+            break;
+            case "password":
+                setRenderComponente(
+                    <Text>
+                        Cambiando password
+                    </Text>
+                );
+                setMostrarModal(true);
+            break;
+            default:
+                setRenderComponente(false);
+                break;
+        }
+    }
+    const opcionesMenu = generarOpciones(seleccionarComponente);
     
     return (
         <View>
@@ -14,24 +49,65 @@ export default function OpcionesCuenta(props) {
                 <ListItem
                     key={index}
                     title={menu.title}
+                    leftIcon={{
+                        type: menu.iconType,
+                        name: menu.iconNameLeft,
+                        color: menu.iconColorLeft,
+                    }}
+                    rightIcon={{
+                        type: menu.iconType,
+                        name: menu.iconNameRight,
+                        color: menu.iconColorRight,
+                    }}
+                    containerStyle={styles.menuItem}
+                    onPress={menu.onPress}
                 />
             ))}
+
+            {renderComponente && (
+                <Modal isVisible={mostrarModal} setIsVisible={setMostrarModal} >
+                    {renderComponente}
+                </Modal>
+            )}
         </View>
     )
 }
 
-function generarOpciones() {
+function generarOpciones(seleccionarComponente) {
     return [
         {
             title : "Cambiar nombre y apellidos",
+            iconType: "material-community",
+            iconNameLeft: "account-circle",
+            iconColorLeft: "#ccc",
+            iconNameRight: "chevron-right",
+            iconColorRight: "#ccc",
+            onPress: () => seleccionarComponente("displayName")
         },
         {
             title: "Cambiar email",
+            iconType: "material-community",
+            iconNameLeft: "at",
+            iconColorLeft: "#ccc",
+            iconNameRight: "chevron-right",
+            iconColorRight: "#ccc",
+            onPress: () => seleccionarComponente("email")
         },
         {
             title: "Cambiar contraseña",
+            iconType: "material-community",
+            iconNameLeft: "lock-reset",
+            iconColorLeft: "#ccc",
+            iconNameRight: "chevron-right",
+            iconColorRight: "#ccc",
+            onPress: () => seleccionarComponente("password")
         },
     ]
 }
 
-const styles= StyleSheet.create({});
+const styles= StyleSheet.create({
+    menuItem : {
+        borderBottomWidth : 1,
+        borderBottomColor: "#e3e3e3",
+    }
+});
